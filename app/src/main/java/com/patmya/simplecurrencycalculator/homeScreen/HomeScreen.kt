d29fbd764f370164d0887e9ca03783450b0e9eeb
Patryk Myalski <patryk.myalski00@gmail.com>
2023-06-22 19:18:07 +0200

    CleanUp

--- app/src/main/java/com/patmya/simplecurrencycalculator/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/patmya/simplecurrencycalculator/homeScreen/HomeScreen.kt
@@ -3,22 +3,17 @@ package com.patmya.simplecurrencycalculator.homeScreen
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.patmya.simplecurrencycalculator.MInputState
-import com.patmya.simplecurrencycalculator.R
 
 
 // On choosing currency use this animation doc: https://developer.android.com/jetpack/compose/animation/composables-modifiers#animatedvisibility
@@ -67,13 +62,6 @@ fun HomeScreen(viewModel: HomeScreenViewModel = viewModel()) {
 
 
 @Composable
-fun ProgressIndicator(size: Int = 40, strokeWidth: Int = 1) {
-    CircularProgressIndicator(
-        modifier = Modifier.size(size.dp), strokeWidth = strokeWidth.dp, strokeCap = StrokeCap.Round, color = MaterialTheme.colors.secondary)
-}
-
-
-@Composable
 fun HomeScreenMainView(viewModel: HomeScreenViewModel) {
 
     // TODO only for development
@@ -102,8 +90,7 @@ fun HomeScreenMainView(viewModel: HomeScreenViewModel) {
         Column(
             modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NumbersNest(
-                onNumberInput = { viewModel.changeInput(it) },
+            NumbersNest(onNumberInput = { viewModel.changeInput(it) },
                 onClear = { viewModel.clearInputs() },
                 onBackSpace = { viewModel.backSpace() })
 
@@ -111,110 +98,6 @@ fun HomeScreenMainView(viewModel: HomeScreenViewModel) {
         }
     }
 
-}
-
-@Composable
-fun NumbersNest(
-    onNumberInput: (number: Char) -> Unit,
-    onClear: () -> Unit,
-    onBackSpace: () -> Unit,
-) {
-
-    val cardBGColor = MaterialTheme.colors.primaryVariant
-
-    Text(
-        text = "Exchange rates are provided by tralalalalalla",
-        color = MaterialTheme.colors.primaryVariant
-    )
-    Row(modifier = Modifier.fillMaxSize()) {
-
-        Column(
-            modifier = Modifier
-                .padding(top = 20.dp)
-                .fillMaxWidth(0.8f)
-                .fillMaxHeight(0.9f),
-            verticalArrangement = Arrangement.SpaceBetween,
-
-            ) {
-            KeyboardRow(numbers = "123") { onNumberInput(it) }
-            KeyboardRow(numbers = "456") { onNumberInput(it) }
-            KeyboardRow(numbers = "789") { onNumberInput(it) }
-            KeyboardRow(numbers = " 0.") { onNumberInput(it) }
-        }
-        Column(
-            modifier = Modifier
-                .padding(top = 20.dp)
-                .fillMaxWidth()
-                .fillMaxHeight(0.9f),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Card(
-                modifier = Modifier
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth(0.8f),
-                shape = CircleShape,
-                elevation = 1.dp,
-                backgroundColor = cardBGColor
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clickable { onClear() },
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "AC",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight(600),
-                        color = MaterialTheme.colors.secondary
-                    )
-                }
-            }
-            Card(
-                modifier = Modifier.fillMaxSize(0.8f),
-                shape = CircleShape,
-                elevation = 1.dp,
-                backgroundColor = cardBGColor
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clickable { onBackSpace() },
-                    painter = painterResource(id = R.drawable.backspace),
-                    contentDescription = "arrow back",
-                    tint = MaterialTheme.colors.secondary
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun KeyboardRow(numbers: String, onClick: (number: Char) -> Unit) {
-
-
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-        for (i in numbers) {
-
-            val modifier = if (i.toString() != " ") Modifier.clickable { onClick(i) } else Modifier
-
-            Card(
-                modifier = Modifier.size(70.dp),
-                shape = CircleShape,
-                elevation = 0.dp,
-                backgroundColor = MaterialTheme.colors.primary
-            ) {
-                Text(
-                    text = i.toString(),
-                    modifier = modifier,
-                    fontSize = 38.sp,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-
-    }
 }
 
 
@@ -238,11 +121,9 @@ fun CurrencyInput(
         Row(
             modifier = Modifier.fillMaxWidth(0.2f), verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.clickable(
-                    interactionSource = interactionSource,
-                    indication = null
-                ) { onChangeCurrency() }) {
+            Row(modifier = Modifier.clickable(
+                interactionSource = interactionSource, indication = null
+            ) { onChangeCurrency() }) {
                 Text(text = "DKK", fontSize = 22.sp) // TODO temporary
                 Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "arrow down")
             }
@@ -252,8 +133,7 @@ fun CurrencyInput(
                 .fillMaxWidth()
                 .clickable(interactionSource = interactionSource, indication = null) {
                     onClick()
-                },
-            horizontalAlignment = Alignment.End
+                }, horizontalAlignment = Alignment.End
         ) {
             Text(
                 text = state.value.value.toString(),
@@ -266,20 +146,3 @@ fun CurrencyInput(
     }
 }
 
-@Composable
-fun TopBar() {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        elevation = 0.dp,
-        backgroundColor = MaterialTheme.colors.primary
-    ) {
-        // TODO maybe change to column and add ad
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = "Currency calculator", fontSize = 20.sp)
-        }
-    }
-}
---
+++ app/src/main/java/com/patmya/simplecurrencycalculator/homeScreen/HomeScreenComponents.kt
@@ -0,0 +1,151 @@
+package com.patmya.simplecurrencycalculator.homeScreen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.patmya.simplecurrencycalculator.R
+
+
+@Composable
+fun TopBar() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 0.dp,
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
+        // TODO maybe change to column and add ad
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Currency calculator", fontSize = 20.sp)
+        }
+    }
+}
+
+
+@Composable
+fun ProgressIndicator(size: Int = 40, strokeWidth: Int = 1) {
+    CircularProgressIndicator(
+        modifier = Modifier.size(size.dp),
+        strokeWidth = strokeWidth.dp,
+        strokeCap = StrokeCap.Round,
+        color = MaterialTheme.colors.secondary
+    )
+}
+
+@Composable
+fun NumbersNest(
+    onNumberInput: (number: Char) -> Unit,
+    onClear: () -> Unit,
+    onBackSpace: () -> Unit,
+) {
+
+    val cardBGColor = MaterialTheme.colors.primaryVariant
+
+    Text(
+        // TODO
+        text = "Exchange rates are provided by tralalalalalla",
+        color = MaterialTheme.colors.primaryVariant
+    )
+    Row(modifier = Modifier.fillMaxSize()) {
+
+        Column(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth(0.8f)
+                .fillMaxHeight(0.9f),
+            verticalArrangement = Arrangement.SpaceBetween,
+
+            ) {
+            KeyboardRow(numbers = "123") { onNumberInput(it) }
+            KeyboardRow(numbers = "456") { onNumberInput(it) }
+            KeyboardRow(numbers = "789") { onNumberInput(it) }
+            KeyboardRow(numbers = " 0.") { onNumberInput(it) }
+        }
+        Column(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(0.9f),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(0.8f),
+                shape = CircleShape,
+                elevation = 1.dp,
+                backgroundColor = cardBGColor
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable { onClear() },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "AC",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight(600),
+                        color = MaterialTheme.colors.secondary
+                    )
+                }
+            }
+            Card(
+                modifier = Modifier.fillMaxSize(0.8f),
+                shape = CircleShape,
+                elevation = 1.dp,
+                backgroundColor = cardBGColor
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable { onBackSpace() },
+                    painter = painterResource(id = R.drawable.backspace),
+                    contentDescription = "arrow back",
+                    tint = MaterialTheme.colors.secondary
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun KeyboardRow(numbers: String, onClick: (number: Char) -> Unit) {
+
+
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        for (i in numbers) {
+
+            val modifier = if (i.toString() != " ") Modifier.clickable { onClick(i) } else Modifier
+
+            Card(
+                modifier = Modifier.size(70.dp),
+                shape = CircleShape,
+                elevation = 0.dp,
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Text(
+                    text = i.toString(),
+                    modifier = modifier,
+                    fontSize = 38.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+
+    }
+}
